7a07436d0c0de77e0808f70b79e6e06461ecbffd
Marc Gomez Iranzo <marc.gomez.developer@gmail.com>
2017-05-25 09:16:37 +0200

    my first commit

---
+++ RockPaperSissors/RockPaperSissors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RockPaperSissors
+//
+//  Created by Marc Gomez Iranzo on 24/5/17.
+//  Copyright © 2017 Marc Gomez Iranzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
